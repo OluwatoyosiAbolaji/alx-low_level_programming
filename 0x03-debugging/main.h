f3d77e88ae10601705a46a90cca1143ab6afffe2
OluwatoyosiAbolaji <oluwatoyosiabolaji25@gmail.com>
2023-02-25 03:13:58 +0100

    C message

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 void positive_or_negative(int i);
 bool func(int a, int b);
-int largest_number(int a, int b, int c);i
+int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 #endif /* MAIN_H */
