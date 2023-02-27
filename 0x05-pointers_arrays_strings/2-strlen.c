2544d6e11b11c8738427859254af1708c4e15333
OluwatoyosiAbolaji <oluwatoyosiabolaji25@gmail.com>
2023-02-28 00:22:28 +0100

    C message

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _strlen(char *s) - returns the length of string
+  * @s: string to be counted
+
+* return: always 0 (success)
+*/
+
+int _strlen(char *s)
+{
+	strlen(*s);
+	return (0);
+}
