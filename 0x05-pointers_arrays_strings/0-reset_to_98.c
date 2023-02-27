1eb8f8730e21ceb164696305507fd1c0af70e936
OluwatoyosiAbolaji <oluwatoyosiabolaji25@gmail.com>
2023-02-27 23:52:24 +0100

    C message

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,13 @@
-#include <main.h>
+#include "holberton.h"
 
+/**
+  * reset_to_98 - Updates the value it points to 98
+  * @n: pointer to n
+  *
+  * Return: void
+  */
 void reset_to_98(int *n)
-	*n = 98
+{
+	*n = 98;
+}
+
