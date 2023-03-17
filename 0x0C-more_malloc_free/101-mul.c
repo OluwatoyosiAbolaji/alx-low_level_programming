0e58e77aa48162a66bbae7930326e2cedfbbf42c
OluwatoyosiAbolaji <oluwatoyosiabolaji25@gmail.com>
2023-03-17 02:30:20 +0100

    malloc project

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,143 +1,58 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _prntstr - prints a string
- *
- * @s: string to print
+ * _isdigit - checks if a string contains only digits
+ * @str: string
+ * Return: 1 (success)
  */
-void _prntstr(char *s)
+int _isdigit(char *str)
 {
-	while (*s)
-		_putchar(*s++);
-}
+	int i;
 
-/**
- * numstrchk - checks arg array to see if the are numeric strings, converts
- * from ascii to byte int, and returns their length. Segfault on null pointer.
- *
- * @s: string to check
- *
- * Return: Length of string. Exit 98 if not numeric.
- */
-long int numstrchk(char *s)
-{
-	long int len = 0;
-
-	if (*s == 0)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		_prntstr("Error\n");
-		exit(98);
-	}
-
-	while (*s)
-	{
-		if (*s < '0' || *s > '9')
+		if (str[i] < 48 || str[i] > 57)
 		{
-			_prntstr("Error\n");
+			printf("Error\n");
 			exit(98);
 		}
-		*s -= '0';
-		len++;
-		s++;
+		i++;
 	}
-	return (len);
+	return (1);
 }
 
 /**
- * _calloc_buffer - allocate a block of memory of size * num and init to '0'
- *
- * @num: number of elements to allocate
- * @size: size of element
- *
- * Return: pointer to allocated space, exit 98 on failure
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: agument variables
+ * Return: 0 (success)
  */
-void *_calloc_buffer(long int num, long int size)
+int main(int argc, char *argv[])
 {
-	void *ret;
-	char *ptr;
+	unsigned long result;
 
-	ret = malloc(num * size);
-	if (ret == 0)
+	if (argc != 3)
 	{
+		printf("Error\n");
 		exit(98);
 	}
 
-	size = size * num;
-	ptr = ret;
-	ptr[--size] = 0;
-	while (size--)
-		ptr[size] = '0';
-
-	return (ret);
-}
-
-/**
- * trimzero - moves pointer position to after last leading 0 in a string,
- * or last zero if all zeros
- *
- * @s: char * we want to move
- *
- * Return: new position
- */
-char *trimzero(char *s)
-{
-	while (*s == '0')
-		if (*(s + 1) != 0)
-			s++;
-		else
-			break;
-	return (s);
-}
-
-/**
- * main - multiply two  positive integer strings of arbitrary size
- *
- * @ac: number of arguments
- * @av: arugments
- *
- * Return: 0 if successful, 98 if failure
- */
-int main(int ac, char **av)
-{
-	long int len1, len2, lenres, i, j;
-	char *res;
-
-	if (ac != 3)
+	if (!_isdigit(argv[1]))
 	{
-		_prntstr("Error\n");
-		return (98);
+		printf("Error\n");
+		exit(98);
 	}
-	av[2] = trimzero(av[2]);
-	av[1] = trimzero(av[1]);
-	if (*av[1] == '0' || *av[2] == '0')
+	if (!_isdigit(argv[2]))
 	{
-		_prntstr("0\n");
-		return (0);
+		printf("Error\n");
+		exit(98);
 	}
-	len1 = numstrchk(av[1]);
-	len2 = numstrchk(av[2]);
-	lenres = len1 + len2;
-	res = _calloc_buffer(lenres + 1, sizeof(char));
 
-	for (i = lenres - 1, len1--; len1 >= 0; len1--, i += len2 - 1)
-		for (j = len2 - 1; j >= 0; j--, i--)
-		{
-			res[i] = (av[1][len1] * av[2][j] % 10) + res[i];
-			res[i - 1] = (av[1][len1] * av[2][j] / 10) + res[i - 1];
-			if (res[i] > '9')
-			{
-				res[i] -= 10;
-				res[i - 1]++;
-			}
-		}
+	result = atol(argv[1]) * atol(argv[2]);
 
-	if (*res == '0')
-		_prntstr(res + 1);
-	else
-		_prntstr(res);
-	_putchar('\n');
-	free(res);
-
+	printf("%lu\n", result);
 	return (0);
 }
