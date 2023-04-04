67f042276f8a7e40b7d75dca63cd0f7ea8cf1f09
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-04-05 00:41:46 +0100

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,15 @@
 #include "lists.h"
+/**
+  *print_listint - prints the int elements of a list
+  *@h: pointer to first node
+  *
+  *Return: number of nodes
+  */
 size_t print_listint(const listint_t *h)
 {
 	size_t number = 0;
 	char convert;
+
 	while (h != NULL)
 	{
 		convert = h->n + '0';
