f4434d0a6148a43f4ff41ddb40e8bf2824b4f7da
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-04-10 18:22:27 +0100

    update

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  *print_binary - prints a decimal in binary format
+  *@n: decimal
+  *Return: void
+  */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
