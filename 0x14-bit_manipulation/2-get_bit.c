b726de9e388d34e5c07e4cbdda9f06cc1013b6f9
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-04-10 19:14:05 +0100

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 /**
   *get_bit - gets the bit of a particular index
   *@n: number in decimal
-  @index: index to be searched from reverse
+  *@index: index to be searched from reverse
   *Return: bit
   */
 int get_bit(unsigned long int n, unsigned int index)
