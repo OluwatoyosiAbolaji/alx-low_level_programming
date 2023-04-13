d0055c0d662043cb7a01aa3715f436cae94707df
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-04-13 12:46:26 +0100

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,33 +1,42 @@
 #include "main.h"
+#define BUFF_SIZE 1024
 /**
   *main - entry point of the program
   *@argc: number of arguments
   *@argv: array of strings
+  *
   *Return: 0 on success
   */
 int main(int argc, char **argv)
 {
 	int file1, file2, i, j, k;
-	char buffer[1024];
+	char buffer[BUFF_SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file1 = open(argv[1], O_RDWR);
-	j = read(file1, buffer, 1024);
-	if (file1 == -1 || j == -1)
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-	k = write(file2, buffer, j);
-	if (file2 == -1 || k == -1)
+	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	while ((j = read(file1, buffer, BUFF_SIZE)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		k = write(file2, buffer, j);
+		if (k != j)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			exit(99);
+		}
+	}
+	if (j == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+		exit(98);
 	}
 	i = close(file1);
 	if (i != 0)
