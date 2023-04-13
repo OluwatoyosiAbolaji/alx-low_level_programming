2bdf09923e4defa7e0d7235d212cfed8c60c5418
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-04-13 13:40:54 +0100

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,6 @@ int main(int argc, char **argv)
 	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((j = read(file1, buffer, BUFF_SIZE)) > 0)
 	{
-		
 		if (j == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
