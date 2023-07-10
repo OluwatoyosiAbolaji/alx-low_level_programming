67948abfdaad4793bb8cfad7f9d53c9059d66a9e
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-07-11 00:30:44 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,40 @@
 #include "main.h"
-#define BUFF_MAX 1024
+
 /**
-  *read_textfile - reads a text file and prints in stdout
-  *@filename: filename to be opened and read
-  *@letters: number of letters to be read
-  *Return: number of letters printed
-  */
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: filename
+ * @letters: actual number of letters it could read and print
+ * Return: 0 if file cannot be opened or filename is NULL or write fails
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file_no, result, j;
-	size_t num_read;
-	char buffer[BUFF_MAX];
+	int file, reed, print;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	file_no = open(filename, O_RDONLY);
-	if (file_no == -1)
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 		return (0);
-	num_read = read(file_no, buffer, letters);
-	if (num_read == -1 || num_read != letters)
-	{
-		close(file_no);
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
 		return (0);
-	}
-	result = write(1, buffer, num_read);
-	j = num_read
-	if (result == -1 || result != j)
-	{
-		close(file_no);
+
+	reed = read(file, buffer, letters);
+	if (reed == -1)
 		return (0);
-	}
-	close(file_no);
-	return (result);
+	buffer[reed] = '\0';
+
+	print = write(STDOUT_FILENO, buffer, reed);
+	if (print == -1 || print != reed)
+		return (0);
+
+	file = close(file);
+	if (file == -1)
+		return (0);
+
+	free(buffer);
+	return (print);
 }
