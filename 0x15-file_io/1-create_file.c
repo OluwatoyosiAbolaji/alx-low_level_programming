33911fb453fd7de3fb6e35cda9a2b3a4f6705e0a
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-07-12 11:16:08 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,14 +10,18 @@ int create_file(const char *filename, char *text_content)
 {
 	int file_no, i, j;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	file_no = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_no == -1)
 		return (-1);
 	for (i = 0; text_content[i];)
 		i++;
-
+	if (text_content == NULL)
+	{
+		close(file_no);
+		return (1);
+	}
 	j = write(file_no, text_content, i);
 	if (j == -1 || j != i)
 	{
