649bd4da278f61b3d002e38abae50292a98fdc31
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-09-07 21:41:01 +0100

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -9,23 +9,20 @@
 void print_ident(const Elf64_Ehdr *header)
 {
 	int i;
-	char *str = NULL;
 
 	printf("ELF Header:\n");
-	printf("	Magic:	");
+	printf("Magic:   ");
 	for (i = 0; i < EI_NIDENT; i++)
-		printf("%02x ", header->e_ident[i]);
-	printf("\n");
-	i = (header->e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
-	printf("	Class:			ELF%d\n", i);
-	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
-		str = "2's complement, little endian";
-	else
-		str = "Unknown";
-	printf("	Data:			%s\n", str);
-	printf("	Version:		%d (current)", header->e_ident[EI_VERSION]);
-	printf("\n");
-	printf("	OS/ABI:			");
+	{
+        	printf("%02x ", header->e_ident[i]);
+	}
+	printf("\nClass:                             ");
+	printf("%s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+	printf("Data:                              ");
+	printf("%s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+	printf("Version:                           ");
+	printf("%u\n", header->e_ident[EI_VERSION]);
+	printf("OS/ABI:                            ");
 	switch (header->e_ident[EI_OSABI])
 	{
 		case ELFOSABI_SYSV:
@@ -37,8 +34,8 @@ void print_ident(const Elf64_Ehdr *header)
 		default:
 			printf("Unknown\n");
 	}
-	printf("	ABI Version:		%d\n", header->e_ident[EI_ABIVERSION]);
-
+	printf("ABI Version:                       ");
+	printf("%u\n", header->e_ident[EI_ABIVERSION]);
 }
 
 /**
@@ -50,7 +47,7 @@ void print_ident(const Elf64_Ehdr *header)
 void print_elf(const Elf64_Ehdr *header)
 {
 	print_ident(header);
-	printf("	Type:			");
+	printf("Type:                              ");
 	switch (header->e_type)
 	{
 		case ET_NONE:
@@ -71,7 +68,8 @@ void print_elf(const Elf64_Ehdr *header)
 		default:
 			printf("Unknown\n");
 	}
-	printf("	Entry point address:	%#lx\n", header->e_entry);
+	printf("Entry point address:               ");
+	printf("%#010x\n", (unsigned int)header->e_entry);
 }
 
 /**
